54eda79334e8e1fb70f523faa671fb8d274fe4cf
Syauqi <sauki084@gmail.com>
2025-11-01 23:09:31 +0700

    design relation table

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Membership extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'plan_id',
+        'start_date',
+        'end_date',
+        'active',
+    ];
+
+    public function plan(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
