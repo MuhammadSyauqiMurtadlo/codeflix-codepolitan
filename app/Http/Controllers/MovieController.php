ad38fc0065726146c03d21b2c96c0212f10e6c0c
Syauqi <sauki084@gmail.com>
2026-03-06 02:52:54 +0700

    Cretae all page movies and load more

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -30,6 +30,19 @@ class MovieController extends Controller Implements HasMiddleware
         ]);
     }
 
+    public function all(Request $request)
+    {
+        $movies = Movie::orderBy('release_date', 'desc')->paginate(8);
+        if ($request->ajax()) {
+            $html = view('components.movie-list', compact('movies'))->render();
+            return response()->json([
+                'html' => $html,
+                'next_page' => $movies->nextPageUrl()  // Mengirim URL halaman berikutnya
+            ]);
+        }
+        return view('movies.all', compact('movies'));
+    }
+
     public function show(Movie $movie)
     {
         $userPlan = Auth::user()->getCurrentPlan();
