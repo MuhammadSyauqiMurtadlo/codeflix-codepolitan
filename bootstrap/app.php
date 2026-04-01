3e855aa1efa0d6d18b5fd58d4cfa0ab557798ac0
Syauqi <sauki084@gmail.com>
2026-04-02 05:56:52 +0700

    Payment Gateway: Prepare Notification and create function callback

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,19 +3,26 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->name('api.')
+                ->group(__DIR__ . '/../routes/api.php');
+        }
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'check.device.limit' => App\Http\Middleware\CheckDeviceLimit::class,
-            'logout.device' => App\Http\Middleware\LogoutDevice::class,
+            'check.device.limit' =>  \App\Http\Middleware\CheckDeviceLimit::class,
+            'logout.device' =>  \App\Http\Middleware\LogoutDevice::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
